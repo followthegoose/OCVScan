6e7d97531f681ed2aa4e973ccfeb527076264a35
followthegoose <60316331+followthegoose@users.noreply.github.com>
2020-02-27 19:06:30 +0300

    Update scan.py

--- scan.py
+++ scan.py
@@ -20,6 +20,7 @@ gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 gray = cv2.GaussianBlur(gray, (5, 5), 0)
 edged = cv2.Canny(gray, 75, 200)
 
+# вывод
 cv2.imshow("Image", image)
 cv2.imshow("Edged", edged)
 cv2.waitKey(0)
@@ -30,6 +31,7 @@ cnts = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 cnts = imutils.grab_contours(cnts)
 cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:5]
 
+# поиск контура
 for c in cnts:
 	# приблизительный контур
 	peri = cv2.arcLength(c, True)
@@ -40,12 +42,16 @@ for c in cnts:
 		break
 
 cv2.drawContours(image, [screenCnt], -1, (0, 255, 0), 2)
+
+# вывод
 cv2.imshow("Outline", image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
 # четырёхточечное преобразование
 warped = four_point_transform(orig, screenCnt.reshape(4, 2) * ratio)
+
+# вывод
 cv2.imshow("Final", imutils.resize(warped, height = 650))
 cv2.waitKey(0)
 
